75a7548c4f8f345ea0a2be5791d2de48a3103e1f
Larissa Guder <lariguder@hotmail.com>
2017-10-11 13:15:04 -0300

    Initial

---
+++ bot_whats_newChat.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 6, 2016
+
+@author: indrajit.n
+'''
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+#driver = webdriver.Firefox()
+
+
+
+driver = webdriver.Chrome()
+driver.get("http://web.whatsapp.com/")
+
+time.sleep(25)
+
+newChat = driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[2]/button')
+newChat.click()
+find = driver.find_element_by_xpath('//input[@title="Search contacts"]')
+find.send_keys('Claudio Scheer')
+time.sleep(3)
+elem = driver.find_element_by_xpath('//span[contains(text(),"Claudio Scheer")]')
+elem.click()
+time.sleep(5)
+elem1 = driver.find_element_by_xpath('//div[@class="pluggable-input-body copyable-text selectable-text"]')
+i = 1
+while True:
+    elem1.send_keys('pruuuuu+ ' + str(i))
+    i = i + 1
+    driver.find_element_by_class_name('compose-btn-send').click()
